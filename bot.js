c00561601c4b7b716d3279de30f5cf703909b01d
王阔 <kuo.wang@funplus.com>
2022-04-02 17:46:57 +0800

    测试

--- bot.js
+++ bot.js
@@ -1,35 +1,32 @@
-const request = require("request");
+const request = require('request')
 const headers = {
-  "Content-Type": "application/json;charset=utf-8",
-};
-const defaultUrl = "https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f";
+    'Content-Type': 'application/json;charset=utf-8',
+}
+const defaultUrl =
+    'https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f'
 const defaultOptions = {
-  msgtype: "text",
-  text: {
-    content: "hello~",
-  },
-};
-
-class Bot {
-  send(url = defaultUrl, json = defaultOptions, callback) {
-    try {
-      let options = {
-        headers,
-        json,
-      };
-      request.post(
-        url,
-        options,
-        function (_error, _response, body) {
-          console.log(`send msg, response: ${JSON.stringify(body)}`);
-        }
-      );
-    } catch (err) {
-      console.error(err);
-      callback && callback()
-      return false;
-    }
-  }
+    msgtype: 'text',
+    text: {
+        content: 'hello~',
+    },
 }
 
-module.exports = Bot;
+class Bot {
+    send(url = defaultUrl, json = defaultOptions, callback) {
+        try {
+            let options = {
+                headers,
+                json,
+            }
+            request.post(url, options, function (_error, _response, body) {
+                console.log(`send msg, response: ${JSON.stringify(body)}`)
+            })
+        } catch (err) {
+            console.error(err)
+            callback && callback()
+            return false
+        }
+    }
+}
+
+module.exports = Bot
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const CronJob = require('cron').CronJob;
-const Bot = require("./bot");
-const bot = new Bot();
-const userList = ["13125026255", "15809244435", "18701452123"]
-const url = "https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f"
+const CronJob = require('cron').CronJob
+const Bot = require('./bot')
+const bot = new Bot()
+// const userList = ["13125026255", "15809244435", "18701452123"]
+const userList = ['18701452123']
 
-const job = new CronJob('*/5 * * * * *', function() {
-  bot.send(url, {
-    msgtype: "text",
-    text: { content: "在干嘛？" },
-    at: { atMobiles: userList },
-  }, job.stop);
-}, null, true, 'America/Los_Angeles');
+const workDayStandUp = '* 50 10-11,14-17 * * 1-5' // 10点到11点 下午2点到5点 50分提醒站起来
+
+const url =
+    'https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f'
+
+const job = new CronJob(
+    workDayStandUp,
+    function () {
+        bot.send(
+            url,
+            {
+                msgtype: 'text',
+                text: { content: '站起来,我的爱牵着山脉！' },
+                at: { atMobiles: userList },
+            },
+            job.stop
+        )
+    },
+    null,
+    true,
+    'America/Los_Angeles'
+)
 // 发送消息
-job.start();
+job.start()
