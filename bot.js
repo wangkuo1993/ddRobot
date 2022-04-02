75e62e2d62b1cb56d3bdb07f34a7ffe5e18c20f8
王阔 <kuo.wang@funplus.com>
2022-04-02 17:10:25 +0800

    新增定时任务

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const request = require("request");
 const headers = {
   "Content-Type": "application/json;charset=utf-8",
 };
-
+const defaultUrl = "https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f";
 const defaultOptions = {
   msgtype: "text",
   text: {
@@ -11,14 +11,14 @@ const defaultOptions = {
 };
 
 class Bot {
-  send(json = defaultOptions) {
+  send(url = defaultUrl, json = defaultOptions, callback) {
     try {
       let options = {
         headers,
         json,
       };
       request.post(
-        "https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f",
+        url,
         options,
         function (_error, _response, body) {
           console.log(`send msg, response: ${JSON.stringify(body)}`);
@@ -26,6 +26,7 @@ class Bot {
       );
     } catch (err) {
       console.error(err);
+      callback && callback()
       return false;
     }
   }
--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const CronJob = require('cron').CronJob;
 const Bot = require("./bot");
 const bot = new Bot();
+const userList = ["13125026255", "15809244435", "18701452123"]
+const url = "https://oapi.dingtalk.com/robot/send?access_token=b1b408f40c5fa2e3340d290335a275e5128d02b42a30a1ca1287c427809dec1f"
 
 const job = new CronJob('*/5 * * * * *', function() {
-  console.log('You will see this message every second');
+  bot.send(url, {
+    msgtype: "text",
+    text: { content: "在干嘛？" },
+    at: { atMobiles: userList },
+  }, job.stop);
 }, null, true, 'America/Los_Angeles');
 // 发送消息
-// bot.send({
-//   msgtype: "text",
-//   text: { content: "在干嘛？" },
-//   at: { atMobiles: ["13125026255", "15809244435"] },
-// });
-
 job.start();
-
-
-setTimeout(() => { 
-  job.stop();
-}, 30000)
