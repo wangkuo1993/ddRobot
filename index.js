4aaa0b93b9663255f7651ad9751a764465068965
王阔 <kuo.wang@funplus.com>
2022-04-02 16:57:19 +0800

    fix: 移除node

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const CronJob = require('cron').CronJob;
 const Bot = require("./bot");
 const bot = new Bot();
 
+const job = new CronJob('*/5 * * * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
 // 发送消息
-bot.send({
-  msgtype: "text",
-  text: { content: "在干嘛？" },
-  at: { atMobiles: ["13125026255", "15809244435"] },
-});
+// bot.send({
+//   msgtype: "text",
+//   text: { content: "在干嘛？" },
+//   at: { atMobiles: ["13125026255", "15809244435"] },
+// });
+
+job.start();
+
+
+setTimeout(() => { 
+  job.stop();
+}, 30000)
